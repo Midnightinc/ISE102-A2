f1dfd05ac80d517e6eebbcb388268b03e14b5560
drakespartan <84439847+drakespartan@users.noreply.github.com>
2025-10-29 00:49:24 +1100

    Password Checks
    Added Logic for a simple salting of the password, along with using HashCode to avoid storing a direct password.
    
    Also added the required properties and constructors for use with this system

--- ISEBank/Bank.cs
+++ ISEBank/Bank.cs
@@ -1,8 +1,35 @@
 ﻿using System;
+using System.Text;
 
 namespace ISEBank
 {
     internal class Bank
     {
+        readonly static Random random = new((int)DateTime.UtcNow.Ticks);
+
+        private static bool PasswordCheck(string passwordEntered, User user)
+        {
+            int passwordAttempt = String.Concat(passwordEntered, user.UserSalt).GetHashCode();
+
+            return (passwordAttempt == user.Password);
+        }
+        
+        /// <summary>
+        /// Generates randomised string, to be added to password before <c>GetHashCode()</c> is used.
+        /// </summary>
+        public static string GenerateSalt(int saltLength = 5)
+        {
+            int minUniCode = 48, maxUnicode = 122;
+            saltLength = Math.Abs(saltLength); //Ensures it's a positive number
+            StringBuilder sb = new();
+
+            for(int i = 0; i < saltLength; i++)
+            {
+                int randNumber = random.Next(minUniCode, maxUnicode + 1);
+                sb.Append((char)randNumber);
+            }
+
+            return sb.ToString();
+        }
     }
 }
--- ISEBank/User.cs
+++ ISEBank/User.cs
@@ -4,5 +4,15 @@ namespace ISEBank
 {
     internal class User
     {
+        public string Username { get; private set; }
+        public int Password { get; private set; }
+        public string UserSalt { get; private set; }
+
+        public User(string username, string password)
+        {
+            Username = username;
+            UserSalt = Bank.GenerateSalt();
+            Password = $"{password}{UserSalt}".GetHashCode();
+        }
     }
 }
